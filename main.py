763562a5fbcb2fe64c0a6eb81576ed3c686a2417
Gabin Houzet <gab1.h@orange.fr>
2025-08-21 12:39:22 +0200

    Initial commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #                                                                                                      #
 # Auteur : Gabin HOUZET (gab1.h@orange.fr)                                                             #
 # Date de création : 17/08/2025                                                                        #
-# Dernière modification le : 22/07/2025                                                                #
+# Dernière modification le : 20/08/2025                                                                #
 # ---------------------------------------------------------------------------------------------------- #
 # Description : Unsteady SIMPLE algorithm on a collocated grid.                                        #
 #                                                                                                      #
@@ -44,6 +44,14 @@
 #        → AcE = max(-Fe, 0), AcW = max(Fw, 0), AcN = max(-Fn, 0), AcS = max(Fs, 0)
 #        → AcP = AcE + AcW + AcN + AcS
 
+# 4. Pressure gradient:
+#    x-momentum :
+#        - ∫∇p dV ≈ -ΔV/Δx ⋅ (pe - pw)
+#        → Qp_u = -Δy⋅ (pe - pw)
+#    y-momentum :
+#        - ∫∇p dV ≈ -ΔV/Δy ⋅ (pn - ps)
+#        → Qp_v = -Δx⋅ (pn - ps)
+
 
 # ==================== < IMPORTS > ===================
 
@@ -58,6 +66,7 @@ import matplotlib.animation as animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from boundary_conditions import *
+from momentum import *
 
 
 # ==================== < PARAMETERS > ===================
@@ -97,7 +106,7 @@ apply_p_bcs(p)
 
 # ==================== < FUNCTIONS > ===================
 
-def build_u_coeffs(u, v, p):
+def build_mom_coeffs(u, v, p, dir):
     """
     Assemble the coefficients for the x-momentum (u-velocity) equation.
 
@@ -146,65 +155,35 @@ def build_u_coeffs(u, v, p):
     aS = aS_c + aS_d
     aP = Qt + aE + aW + aN + aS
 
-    # Pressure gradient term:
-    Qp = -dy * (p[1:-1, 2:] - p[1:-1, :-2]) / 2
-
-    b = Qt*u[1:-1, 1:-1] + Qp
-
-    return aP, aE, aW, aN, aS, b
-
-def build_v_coeffs(u, v, p):
-    """
-    Assemble the coefficients for the y-momentum (v-velocity) equation.
-
-    Returns aP, aE, aW, aN, aS, b
-    """
-
-    # Initialize coefficients arrays:
-    aP = np.zeros((Ny, Nx))
-    aE = np.zeros((Ny, Nx))
-    aW = np.zeros((Ny, Nx))
-    aN = np.zeros((Ny, Nx))
-    aS = np.zeros((Ny, Nx))
-    b  = np.zeros((Ny, Nx))
-
-    # Diffusion terms:
-    aE_d, aW_d, aN_d, aS_d = mu*dy/dx, mu*dy/dx, mu*dx/dy, mu*dx/dy
-
-    # Implicit time term:
-    Qt = rho*dx*dy/dt
-
-    # Initialize face velocities arrays:
-    ue = np.zeros_like(u)
-    uw = np.zeros_like(u)
-    vn = np.zeros_like(v)
-    vs = np.zeros_like(v)
-
-    # Face velocities:
-    ue = 0.5*(u[1:-1, 1:-1] + u[1:-1, 2:])
-    uw = 0.5*(u[1:-1, :-2] + u[1:-1, 1:-1])
-    vn = 0.5*(v[1:-1, 1:-1] + v[2:, 1:-1])
-    vs = 0.5*(v[:-2, 1:-1] + v[1:-1, 1:-1])
-
-    # Mass fluxes through faces:
-    Fe, Fw, Fn, Fs = rho*ue*dy, rho*uw*dy, rho*vn*dx, rho*vs*dx
-
-    # Convection coefficients (Upwind):
-    aE_c = np.maximum(-Fe, 0)
-    aW_c = np.maximum(Fw, 0)
-    aN_c = np.maximum(-Fn, 0)
-    aS_c = np.maximum(Fs, 0)
-
-    # Full coefficient arrays:
-    aE = aE_c + aE_d
-    aW = aW_c + aW_d
-    aN = aN_c + aN_d
-    aS = aS_c + aS_d
-    aP = Qt + aE + aW + aN + aS
-
-    # Pressure gradient term:
-    Qp = -dx * (p[2:, 1:-1] - p[:-2, 1:-1]) / 2
-
-    b = Qt*v[1:-1, 1:-1] + Qp
+    if dir == 'x':
+        Qp = -dy * (p[1:-1, 2:] - p[1:-1, :-2]) / 2    # Pressure gradient term
+        b = Qt*u[1:-1, 1:-1] + Qp
+    elif dir == 'y':
+        Qp = -dx * (p[2:, 1:-1] - p[:-2, 1:-1]) / 2    # Pressure gradient term
+        b = Qt*v[1:-1, 1:-1] + Qp
 
     return aP, aE, aW, aN, aS, b
+
+
+def assemble_sparse_matrix(aP, aE, aW, aN, aS):
+    d0 = aP.reshape(Nx*Ny)
+    de = aE.reshape(Nx*Ny)[:-1]
+    dw = aW.reshape(Nx*Ny)[1:]
+    dn = aN.reshape(Nx*Ny)[:-Nx]
+    ds = aS.reshape(Nx*Ny)[Nx:]
+
+    A = scipy.sparse.diags([d0, de, dw, dn, ds], [0, 1, -1, Nx, -Nx], format='csr')
+
+    # plt.matshow(A.toarray())
+    # plt.show()
+
+    return A
+
+
+# ==================== < MAIN TIME LOOP > ===================
+
+aP_u, aE_u, aW_u, aN_u, aS_u, b_u = build_mom_coeffs(u, v, p, dir='x')
+aP_v, aE_v, aW_v, aN_v, aS_v, b_v = build_mom_coeffs(u, v, p, dir='y')
+
+A_u = assemble_sparse_matrix(aP_u, aE_u, aW_u, aN_u, aS_u)
+A_v = assemble_sparse_matrix(aP_v, aE_v, aW_v, aN_v, aS_v)
